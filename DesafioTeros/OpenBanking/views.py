397884dfc5a2266a1928b236be0ba04c9f48841d
Lucas <lucasnego0142@gmail.com>
2021-11-28 17:48:05 -0300

    Finalização das Funcionalidades: Atualiza a cada 5 minutos e Frontend

--- DesafioTeros/OpenBanking/views.py
+++ DesafioTeros/OpenBanking/views.py
@@ -4,8 +4,10 @@ from .models import Servers
 from django_q.tasks import async_task
 
 def index(request):
+    if request.POST.get('deletar_db'):
+        Servers.objects.all().delete()
+        objeto_list = None
     if request.method == 'GET':
         objeto_list = Servers.objects.all().order_by('nome_empresa')
-        async_task("OpenBanking.tasks.atualiza_bd")
     context = {'objeto_list':objeto_list}
     return render(request, "index.html",context)
